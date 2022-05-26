93d9f9c2e5817b9a604e6bc1649751de4dde64b7
DANYELKAR <danyelkar1905@gmail.com>
2022-05-26 17:35:07 +0300

    Add TimeAgo and order the posts by timing

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,16 +1,22 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import PostAuthor from "./PostAuthor";
+import TimeAgo from "./TimeAgo";
 
 const PostList = () => {
   const posts = useSelector((state) => state.postsAbc);
 
-  const renderedPosts = posts.map((post) => (
+  const orderedPosts = posts
+    .slice()
+    .sort((a, b) => b.date.localeCompare(a.date));
+
+  const renderedPosts = orderedPosts.map((post) => (
     <article key={post.id}>
       <h3>{post.title}</h3>
       <p>{post.content.substring(0, 100)}</p>
       <p className="postCredit">
         <PostAuthor userId={post.userId} />
+        <TimeAgo timestamp={post.date} />
       </p>
     </article>
   ));
--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,16 +1,19 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit";
+import { sub } from "date-fns/esm";
 
 const initialState = [
   {
     id: "1",
     title: "Learning redux",
     content: "I have heard amazing things about that redux tools :)",
+    date: sub(new Date(), { minutes: 10 }).toISOString(),
   },
   {
     id: "2",
     title: "crypto summit",
     content:
       "Learning own your money for financial freedom so i think crpyto is the best way",
+    date: sub(new Date(), { minutes: 5 }).toISOString(),
   },
 ];
 
@@ -28,6 +31,7 @@ const postsSlice = createSlice({
             id: nanoid(),
             title,
             content,
+            date: new Date().toISOString(),
             userId,
           },
         };
